fa74195673ea66408ed41612d4b1df64c2b56616
Bernabe Ibarra <bibarra@uw.edu>
2019-02-23 10:23:29 -0800

    added list function
    First the directory, and then the file witht the function specified on the assingment page

---
+++ scripts/list_info.R
@@ -0,0 +1,7 @@
+# A function that takes in a dataset and returns a list of info about it:
+intro_function <- function(dataset) {
+    ret <- list()
+    ret$length <- length(dataset)
+    # do some more interesting stuff
+    return (ret)
+} 
