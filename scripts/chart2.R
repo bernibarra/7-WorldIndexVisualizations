329f6733452645409b20bddd2ec4fb65298bd30b
Feng Yu Yeh <fengy17@uw.edu>
2019-02-26 08:15:14 -0800

    add chart 2, close #3

---
+++ scripts/chart2.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(plotly)
+
+# Make an interactive plot that shows the relationship between the percentage
+# of GDP that's spent on education expenditure and unemployment rate
+
+chart_2 <- function(data_frame){
+  data_for_graph <- data_frame %>%
+    select(country_name, education_expenditure_pct_of_gdp, unemployment_pct) %>%
+    rename(country = country_name,
+           education_expenditure =
+             education_expenditure_pct_of_gdp)
+
+  interactive_plot <- plot_ly(
+    data = data_for_graph, # data frame to show
+    x = ~education_expenditure, # variable for the x-axis
+    y = ~unemployment_pct, # variable for the y-axis
+    marker = list(size = 10),
+    hoverinfo = "text",
+    text = ~paste0("Country: ", country,
+                  "<br>Percent GDP in Education Expenditure: ",
+                  round(education_expenditure, 2), "%",
+                  "<br>Unemployment Rate: ",
+                  unemployment_pct, "%")
+  ) %>%
+    layout(
+      title = "Education Expenditure vs Unemployment Rate",
+      xaxis = list(title = "Percentage of GDP in Education Expenditure"),
+      yaxis = list(title = "Unemployment Rate")
+    )
+}
