476127e973eb01642a5c5ee978b1e587c607f9ed
Feng Yu Yeh <fengy17@uw.edu>
2019-02-26 21:18:25 -0800

    edit format from lint

--- scripts/summary_calculations.R
+++ scripts/summary_calculations.R
@@ -8,17 +8,17 @@ summary_calculations <- function(dataset) {
                                 health_expenditure_per_person =
                                   as.numeric(health_expenditure_per_person)
                                 )
-  
-  results$avg_health_expenditure_pct <- transformed_data %>% 
-    select(health_expenditure_pct_of_gdp) %>% 
+
+  results$avg_health_expenditure_pct <- transformed_data %>%
+    select(health_expenditure_pct_of_gdp) %>%
     summarise(avg = mean(health_expenditure_pct_of_gdp, na.rm = TRUE))
-  
+
   results$avg_world_happiness <- transformed_data %>%
-    select(world_happiness_report_score) %>% 
+    select(world_happiness_report_score) %>%
     summarise(avg = mean(world_happiness_report_score, na.rm = TRUE))
-  
-  results$avg_health_expenditure_per_person <- transformed_data %>% 
-    select(health_expenditure_per_person) %>% 
+
+  results$avg_health_expenditure_per_person <- transformed_data %>%
+    select(health_expenditure_per_person) %>%
     summarise(avg = mean(health_expenditure_per_person, na.rm = TRUE))
 
   return (results)
